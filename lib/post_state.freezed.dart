180770ae1141747d75fdab5ff9cb00970e91919f
HikaruKusanagi <hikarugp24@gmail.com>
2025-05-16 00:05:33 +0900

    no message

---
+++ lib/post_state.freezed.dart
@@ -0,0 +1,184 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'post_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$PostState {
+  bool get isLoading => throw _privateConstructorUsedError;
+  Uint8List? get imageBitmap => throw _privateConstructorUsedError;
+  String get errorMessage => throw _privateConstructorUsedError;
+
+  /// Create a copy of PostState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $PostStateCopyWith<PostState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $PostStateCopyWith<$Res> {
+  factory $PostStateCopyWith(PostState value, $Res Function(PostState) then) =
+      _$PostStateCopyWithImpl<$Res, PostState>;
+  @useResult
+  $Res call({bool isLoading, Uint8List? imageBitmap, String errorMessage});
+}
+
+/// @nodoc
+class _$PostStateCopyWithImpl<$Res, $Val extends PostState>
+    implements $PostStateCopyWith<$Res> {
+  _$PostStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of PostState
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? isLoading = null,
+    Object? imageBitmap = freezed,
+    Object? errorMessage = null,
+  }) {
+    return _then(_value.copyWith(
+      isLoading: null == isLoading
+          ? _value.isLoading
+          : isLoading // ignore: cast_nullable_to_non_nullable
+              as bool,
+      imageBitmap: freezed == imageBitmap
+          ? _value.imageBitmap
+          : imageBitmap // ignore: cast_nullable_to_non_nullable
+              as Uint8List?,
+      errorMessage: null == errorMessage
+          ? _value.errorMessage
+          : errorMessage // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$PostStateImplCopyWith<$Res>
+    implements $PostStateCopyWith<$Res> {
+  factory _$$PostStateImplCopyWith(
+          _$PostStateImpl value, $Res Function(_$PostStateImpl) then) =
+      __$$PostStateImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({bool isLoading, Uint8List? imageBitmap, String errorMessage});
+}
+
+/// @nodoc
+class __$$PostStateImplCopyWithImpl<$Res>
+    extends _$PostStateCopyWithImpl<$Res, _$PostStateImpl>
+    implements _$$PostStateImplCopyWith<$Res> {
+  __$$PostStateImplCopyWithImpl(
+      _$PostStateImpl _value, $Res Function(_$PostStateImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of PostState
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? isLoading = null,
+    Object? imageBitmap = freezed,
+    Object? errorMessage = null,
+  }) {
+    return _then(_$PostStateImpl(
+      isLoading: null == isLoading
+          ? _value.isLoading
+          : isLoading // ignore: cast_nullable_to_non_nullable
+              as bool,
+      imageBitmap: freezed == imageBitmap
+          ? _value.imageBitmap
+          : imageBitmap // ignore: cast_nullable_to_non_nullable
+              as Uint8List?,
+      errorMessage: null == errorMessage
+          ? _value.errorMessage
+          : errorMessage // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$PostStateImpl implements _PostState {
+  const _$PostStateImpl(
+      {this.isLoading = false, this.imageBitmap, this.errorMessage = ''});
+
+  @override
+  @JsonKey()
+  final bool isLoading;
+  @override
+  final Uint8List? imageBitmap;
+  @override
+  @JsonKey()
+  final String errorMessage;
+
+  @override
+  String toString() {
+    return 'PostState(isLoading: $isLoading, imageBitmap: $imageBitmap, errorMessage: $errorMessage)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$PostStateImpl &&
+            (identical(other.isLoading, isLoading) ||
+                other.isLoading == isLoading) &&
+            const DeepCollectionEquality()
+                .equals(other.imageBitmap, imageBitmap) &&
+            (identical(other.errorMessage, errorMessage) ||
+                other.errorMessage == errorMessage));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, isLoading,
+      const DeepCollectionEquality().hash(imageBitmap), errorMessage);
+
+  /// Create a copy of PostState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$PostStateImplCopyWith<_$PostStateImpl> get copyWith =>
+      __$$PostStateImplCopyWithImpl<_$PostStateImpl>(this, _$identity);
+}
+
+abstract class _PostState implements PostState {
+  const factory _PostState(
+      {final bool isLoading,
+      final Uint8List? imageBitmap,
+      final String errorMessage}) = _$PostStateImpl;
+
+  @override
+  bool get isLoading;
+  @override
+  Uint8List? get imageBitmap;
+  @override
+  String get errorMessage;
+
+  /// Create a copy of PostState
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$PostStateImplCopyWith<_$PostStateImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
